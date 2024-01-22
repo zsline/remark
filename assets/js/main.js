dd210701c542fca55022f3251b6b361166243ef7
zsline <sergei.dereka@gmail.com>
2024-01-22 20:21:10 +0200

    22-01-24-20

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,17 @@
     //Записываем, сколько проскроллено по вертикали
     let scrollpos = window.scrollY;
 
-    const header = document.querySelector(".header");
-    const scroller = document.querySelector(".scroll-top");
+    const header = document.querySelector('.header');
+    const scroller = document.querySelector('.scroll-top');
   
     //Сколько пикселей нужно проскролить, чтобы добавить класс. Можете изменить значение
     const scrollChange = 2860;
   
     //Функция, которая будет добавлять класс
-    const add_class_on_scroll = () => header.classList.add("scroll");
-    const add_clas_scroll = () => scroller.classList.add("scroll");
-    const remove_class_on_scroll = () => header.classList.remove("scroll");
-    const remove_class_scroll = () => scroller.classList.remove("scroll");
+    const add_class_on_scroll = () => header.classList.add('scroll');
+    const add_clas_scroll = () => scroller.classList.add('scroll');
+    const remove_class_on_scroll = () => header.classList.remove('scroll');
+    const remove_class_scroll = () => scroller.classList.remove('scroll');
   
     //Отслеживаем событие "скролл"
     if(header){
@@ -21,20 +21,24 @@
         scrollpos = window.scrollY;
   
       //Если прокрутили больше, чем мы указали в переменной scrollChange, то выполняется функция добавления класса
-          if (scrollpos > scrollChange) { add_class_on_scroll(), add_clas_scroll() }
-        if (scrollpos < scrollChange) { remove_class_on_scroll(), remove_class_scroll() }
+          if (scrollpos >= scrollChange) { add_class_on_scroll(), add_clas_scroll() }
+        if (scrollpos <= scrollChange) { remove_class_on_scroll(), remove_class_scroll() }
       })
     }
+
+
     document.querySelector('.scroll-top').onclick = () => {
         window.scrollTo(pageYOffset, 0);
     }
+
+
+
 const navMenu = document.querySelector('.header__nav ul');
 const navMenuItem = navMenu.querySelectorAll('li');
 let widthItem = navMenu.offsetWidth / navMenuItem.length;
 navMenuItem.forEach((el) => {
     el.style.width = widthItem + 'px';
 });
-console.log(widthItem);
 
 
 // Video 1 ===================
@@ -62,27 +66,34 @@ const topSlider = new Swiper('.top__slider',{
       },
 
 });
-const customerSlider = new Swiper('.customer__slider',{
-    spaceBetween: 40,
-    slidesPerView: 1,
-    effect: 'slide',
-    speed: 1500,
-    autoplay: true,
-    navigation: {
-        nextEl: '.customer__slider--next',
-        prevEl: '.customer__slider--prev',
-      },
+if(document.querySelector('.customer__slider')){
+    const customerSlider = new Swiper('.customer__slider',{
+        spaceBetween: 40,
+        slidesPerView: 1,
+        effect: 'slide',
+        speed: 1500,
+        autoplay: true,
+        navigation: {
+            nextEl: '.customer__slider--next',
+            prevEl: '.customer__slider--prev',
+        },
 
-});
-const pauseSlider = document.querySelector('.top__slider--pause');
-const playSlider = document.querySelector('.top__slider--play');
-pauseSlider.addEventListener('click', function() {
-    topSlider.autoplay.stop();
-    this.style.display = 'none';
-    playSlider.style.display = 'block';
-})
-playSlider.addEventListener('click', function() {
-    topSlider.autoplay.start();
-    this.style.display = 'none';
-    pauseSlider.style.display = 'block';
-})
+    });
+    const pauseSlider = document.querySelector('.top__slider--pause');
+    const playSlider = document.querySelector('.top__slider--play');
+    if(pauseSlider){
+            pauseSlider.addEventListener('click', function() {
+            topSlider.autoplay.stop();
+            this.style.display = 'none';
+            playSlider.style.display = 'block';
+        }) 
+    }
+    if(playSlider){
+        playSlider.addEventListener('click', function() {
+        topSlider.autoplay.start();
+        this.style.display = 'none';
+        pauseSlider.style.display = 'block';
+    })   
+    }
+
+}
